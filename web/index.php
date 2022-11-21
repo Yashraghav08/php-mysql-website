c702c006d7eb6609cfef93a62b85b2bcaf9d69ea
Yashraghav08 <68010966+Yashraghav08@users.noreply.github.com>
2022-11-21 11:22:20 +0530

    Update index.php

--- web/index.php
+++ web/index.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bloco de Notas</title>
+    <title>My Website</title>
     <meta name="Description of person" content="">
     <meta name="author" content="77896,77907,77969">
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet">
